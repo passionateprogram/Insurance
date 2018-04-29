762987588f17d9f28d31346e878ba23b1f97aab2
Selvam <passionateprogrammer@gmail.com>
2018-04-29 10:13:40 +0530

    Data access test project is added

---
+++ Insurance/DataAccess/InsuranceDataAccess/crud/reader/TblMotorDetailReader.cs
@@ -0,0 +1,29 @@
+﻿using InsuranceDataAccess.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceDataAccess.crud.reader
+{
+    public class TblMotorDetailReader
+    {
+        public TblMotorDetailReader()
+        {
+
+        }
+
+        public List<string> ReadALLBrands()
+        {
+            var result = new List<string>();
+
+            using (var db = new hackathonteam289_dbEntities1())
+            {
+                result = db.tblMotorDetails.Select(i=>i.Brand).Distinct().ToList();                
+            }
+
+            return result;
+        }
+    }
+}
