18dad0ae04c7733313c69ef5b64651f30a125212
passionateprogram <38701729+passionateprogram@users.noreply.github.com>
2018-05-03 14:55:23 +0530

    DataAccess Layer is updated

--- Insurance/DataAccess/InsuranceDataAccess/crud/reader/TblMotorDetailReader.cs
+++ Insurance/DataAccess/InsuranceDataAccess/crud/reader/TblMotorDetailReader.cs
@@ -1,29 +1,97 @@
 ﻿using InsuranceDataAccess.model;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InsuranceDataAccess.crud.reader
 {
     public class TblMotorDetailReader
     {
-        public TblMotorDetailReader()
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="vehicleType"></param>
+        /// <returns></returns>
+        public List<string> GetVehicleBrands(int vehicleType)
         {
-
-        }
-
-        public List<string> ReadALLBrands()
-        {
-            var result = new List<string>();
-
             using (var db = new hackathonteam289_dbEntities1())
             {
-                result = db.tblMotorDetails.Select(i=>i.Brand).Distinct().ToList();                
+                return db.tblMotorDetails.Where(i => i.VehicleType == vehicleType).Select(i => i.Brand).Distinct().ToList();
             }
-
-            return result;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="vehicleType"></param>
+        /// <param name="brandName"></param>
+        /// <returns></returns>
+        public List<string> GetVehicleModels(int vehicleType, string brandName)
+        {
+            using (var db = new hackathonteam289_dbEntities1())
+            {
+                return db.tblMotorDetails.Where(i => i.VehicleType == vehicleType && i.Brand.ToLower() == brandName.ToLower()).Select(i => i.Model).Distinct().ToList();
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="vehicleType"></param>
+        /// <param name="brandName"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public List<int> GetVehicleManufactureYear(int vehicleType, string brandName, string model)
+        {
+            using (var db = new hackathonteam289_dbEntities1())
+            {
+                return db.tblMotorDetails.Where(i => i.VehicleType == vehicleType && i.Brand.ToLower() == brandName.ToLower() && i.Model.ToLower() == model.ToLower()).Select(i => i.Make).Distinct().ToList();
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="vehicleType"></param>
+        /// <param name="brandName"></param>
+        /// <param name="model"></param>
+        /// <param name="year"></param>
+        /// <returns></returns>
+        public decimal? GetVehicleAmount(int vehicleType, string brandName, string model, int year)
+        {
+            using (var db = new hackathonteam289_dbEntities1())
+            {
+                var vehicle = db.tblMotorDetails.FirstOrDefault(i => i.VehicleType == vehicleType && i.Brand.ToLower() == brandName.ToLower() && i.Model.ToLower() == model.ToLower() && i.Make == year);
+                if (vehicle != null)
+                    return vehicle.BaselineAmount;
+                else
+                    return null;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="vehicleType"></param>
+        /// <returns></returns>
+        public List<tblMotorVendorDetail> GetVendorDetails(int vehicleType)
+        {
+            using (var db = new hackathonteam289_dbEntities1())
+            {
+                return db.tblMotorVendorDetails.Where(i => i.VehicleType == vehicleType).ToList();
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="vehicleType"></param>
+        /// <param name="venderName"></param>
+        /// <returns></returns>
+        public string GetVendorFeatures(int vehicleType, string venderName)
+        {
+            using (var db = new hackathonteam289_dbEntities1())
+            {
+                var vendor = db.tblMotorVendorDetails.FirstOrDefault(i => i.VehicleType == vehicleType && i.VendorName.ToLower() == venderName.ToLower());
+                if (vendor != null)
+                    return vendor.Features;
+                else
+                    return null;
+            }
         }
     }
 }
--- Insurance/DataAccess/InsuranceDataAccessTest/InsuranceDataAccessTest/Program.cs
+++ Insurance/DataAccess/InsuranceDataAccessTest/InsuranceDataAccessTest/Program.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace InsuranceDataAccessTest
+﻿namespace InsuranceDataAccessTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-            new InsuranceDataAccess.crud.reader.TblMotorDetailReader().ReadALLBrands();
+            //new InsuranceDataAccess.crud.reader.TblMotorDetailReader().ReadALLBrands();
         }
     }
 }
